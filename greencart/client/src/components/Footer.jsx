2312a05016dda0e99df55b2429c65d22763d1c8a
veeravasanthan <153995892+veeravasanthan@users.noreply.github.com>
2025-07-21 20:03:40 +0530

    Next added origin

--- greencart/client/src/components/Footer.jsx
+++ greencart/client/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright {new Date().getFullYear()} © GreatStack.dev All Right Reserved.
+                Copyright {new Date().getFullYear()} ©  All Right Reserved.
             </p>
         </div>
     );
